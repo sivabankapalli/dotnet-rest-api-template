2e26728c497a0c20035db177de8d70b5fe6a83a1
siva-bankapalli <siva.bankapalli@yahoo.com>
2025-06-29 21:58:20 +1200

    service url

--- scripts/sit.sh
+++ scripts/sit.sh
@@ -12,7 +12,7 @@ echo "Starting Kubernetes Deployment"
 chmod +x ./scripts/deploy.sh
 ./scripts/deploy.sh
 
-export FDAS_SERVICE_URL="${NAMESPACE_NAME}.myorg.com"
+export SERVICE_URL="${NAMESPACE_NAME}.myorg.com"
 
 echo "Running Integration tests ...."
 #tests here..
